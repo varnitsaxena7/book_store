2cc5300f13683fd5b4dc23a967e64209b1f6d00e
varnitsaxena7 <saxenavarnit.code7@gmail.com>
2024-02-26 05:08:36 +0530

    Added

---
+++ Backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://saxenavarnitcode7:hello123@cluster0.4tu17jt.mongodb.net/BookStore?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
